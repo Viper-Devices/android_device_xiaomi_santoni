1fa27f596d4f35d69c581eec2a4c3d271e5b94d0
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2018-06-04 14:25:29 +0000

    Revert "santoni: rootdir: enable adaptive lowmemorykiller"
    This reverts commit e541479be620f304718472f355370106ea2739c2.

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -302,9 +302,6 @@ else
     SWAP_ENABLE_THRESHOLD=1048576
     swap_enable=`getprop ro.vendor.qti.config.swap`
 
-    # Enabling Adaptive Lowmemorykiller
-    echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-
     if [ -f /sys/devices/soc0/soc_id ]; then
         soc_id=`cat /sys/devices/soc0/soc_id`
     else

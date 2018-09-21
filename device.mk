7ac95dc897b8ac4646f4e9fde1b81ecb3d7e7282
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2018-09-21 09:30:52 +0000

    santoni: usb: import usb HAL from caf from LA.UM.7.6.r1-02500-89xx.0

--- device.mk
+++ device.mk
@@ -416,7 +416,8 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service \
+    android.hardware.usb.gadget@1.0-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
